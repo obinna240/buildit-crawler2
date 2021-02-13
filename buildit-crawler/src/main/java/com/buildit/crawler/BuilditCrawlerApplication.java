a2e4aac17f8fe585b70ad8e3634e14b26527a5bf
Obinna Onyimadu <obinna240@yahoo.co.uk>
2021-02-13 18:15:38 +0000

    Starter project

---
+++ buildit-crawler/src/main/java/com/buildit/crawler/BuilditCrawlerApplication.java
@@ -0,0 +1,13 @@
+package com.buildit.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuilditCrawlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuilditCrawlerApplication.class, args);
+	}
+
+}
---
+++ buildit-crawler/src/test/java/com/buildit/crawler/BuilditCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buildit.crawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuilditCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
