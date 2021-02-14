9ec1c7002e0cbfbf02b9b61b2ce9534313fc590c
Obinna Onyimadu <obinna240@yahoo.co.uk>
2021-02-14 07:26:03 +0000

    Compare domains test

--- buildit-crawler/src/main/java/com/buildit/crawler/Crawler.java
+++ buildit-crawler/src/main/java/com/buildit/crawler/Crawler.java
@@ -2,25 +2,75 @@ package com.buildit.crawler;
 
 import com.buildit.crawler.exceptions.InvalidCrawlURLException;
 import com.buildit.crawler.exceptions.InvalidDepthException;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.validator.routines.UrlValidator;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.util.*;
 
 public final class Crawler {
 
-    final static int DEFAULT_DEPTH = 3;
-    final Map<String, Set<String>> crawledPages;
+    private final static int DEFAULT_DEPTH = 3;
+    private final Map<String, Set<String>> indexMap;
+    private final Set<String> urlIndex;
+    private final static String HTML_ANCHOR_REF = "a[href]";
+    private final static String HTML_IMG_REF = "img";
 
     public Crawler() {
-         this.crawledPages = new LinkedHashMap<String, Set<String>>();
+         this.indexMap = new LinkedHashMap<>();
+         this.urlIndex = new HashSet<>();
     }
 
-    public void crawl(String url, int depth) {
+    public void crawl(String url, int crawlDepth) {
         if (!validateUrl(url)) {
             throw new InvalidCrawlURLException("The URL is invalid");
         }
-        if( depth<0 ) {
+        if( crawlDepth<0 ) {
             throw new InvalidDepthException("depth cannot be a negative number");
+        }
+        if(!urlIndex.contains(url) && crawlDepth < DEFAULT_DEPTH) {
+            try {
+                urlIndex.add(url);
+                Document doc = Jsoup.connect(url).get();
+                Elements links = doc.select(HTML_ANCHOR_REF);
+                Elements imageLinks = doc.select(HTML_IMG_REF);
+                links.addAll(imageLinks);
+                crawlDepth++;
+
+                links.stream().forEach(link -> {
+                    populateIndex(indexMap, url, link);
+//                    if(link)
+                });
+            }
+            catch(Exception ex) {
+
+            }
+        }
+    }
+
+    public void crawl(String url){
+
+    }
+
+    private void populateIndex(Map<String, Set<String>> index, String url, Element element){
+        String href = element.attr("abs:href");
+        String imgsrc = element.attr("abs:src");
+        if(index.containsKey(url)) {
+            if (StringUtils.isNotBlank(href)) {
+                index.get(url).add(href);
+            }
+            if (StringUtils.isNotBlank(imgsrc)) {
+                index.get(url).add(imgsrc);
+            }
+        }
+        else {
+            Set<String> index_set = new LinkedHashSet<>();
+            index_set.add(href);
+            index_set.add(imgsrc);
+            index.put(url, index_set);
         }
     }
 
--- buildit-crawler/src/test/java/com/buildit/crawler/CrawlerTest.java
+++ buildit-crawler/src/test/java/com/buildit/crawler/CrawlerTest.java
@@ -1,13 +1,18 @@
 package com.buildit.crawler;
 
 import com.buildit.crawler.exceptions.InvalidCrawlURLException;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.validator.routines.UrlValidator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import com.buildit.crawler.exceptions.InvalidDepthException;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CrawlerTest {
@@ -39,10 +44,43 @@ public class CrawlerTest {
 
     }
 
+    @Test
     public void willReturnExceptionForInvalidOrNullURL() {
         assertThrows(InvalidCrawlURLException.class, () -> {
             crawler.crawl("invalidURL", 1);
         });
+    }
+
+    @Test
+    public void willReturnGoogleAsDomainName() throws Exception {
+        String test = "http://www.google.co.uk#1234";
+        URI uri = new URI(test);
+        String domain = uri.getHost();
+        String dd = domain.startsWith("www.")?domain.substring(4): domain;
+        String domainName = StringUtils.substringBefore(dd, ".");
+        assertEquals("google", domainName);
+    }
+
+    @Test
+    public void willReturnFalseOnCheckingThatDomainNamesAreNotEqual() throws Exception {
+        String url1 = "http://www.google.co.uk#1234";
+        String url2 = "https://www.abcnews.com";
+        URI uri = new URI(url1);
+        String domain = uri.getHost();
+        String dd = domain.startsWith("www.")?domain.substring(4): domain;
+        String domainName = StringUtils.substringBefore(dd, ".");
+
+        URI uri2 = new URI(url2);
+        String domain2 = uri2.getHost();
+        String dd2= domain2.startsWith("www.")?domain2.substring(4): domain2;
+        String domainName2 = StringUtils.substringBefore(dd2, ".");
+
+        boolean compare = domainName.equalsIgnoreCase(domainName2);
+        assertThat(compare).isFalse();
+    }
+
+    public void willReturnMapOfLinks(){
+//        assertEquals(3, crawler.crawl(dummyUrl)).size()) ;
     }
 
 
